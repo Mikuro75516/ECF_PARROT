1c1fb57a1b18f2982ee5f7d3eef77ee615848cc6
Mikuro75516 <melhouillon@outlook.fr>
2024-02-20 00:20:27 +0100

    Premier commit

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    // Optimisation : Utilisation d'une seule fonction pour gérer plusieurs événements sur les liens
+    $(".voiture.en.avant a").on('click mouseenter mouseover', function(e) {
+        switch(e.type) {
+            case 'click':
+                e.preventDefault();
+                var url = $(this).attr('href');
+                window.open(url, '_blank');
+                e.stopPropagation();
+                break;
+            case 'mouseenter':
+            case 'mouseover':
+                $(this).removeAttr('title');
+                e.stopPropagation();
+                break;
+        }
+    });
+
+    function isElementInView(element) {
+        var $el = $(element);
+        var elementTop = $el.offset().top;
+        var elementBottom = elementTop + $el.outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    }
+
+    function animateElements() {
+        $('.col').each(function() {
+            var $this = $(this);
+            var transformProperties = isElementInView($this) ? {
+                transform: 'translateX(0) scale(1) rotate(0deg)',
+                opacity: 1
+            } : {
+                transform: `translateX(${ $this.index() % 2 === 0 ? '-200px' : '200px' }) scale(0.8) rotate(${ $this.index() % 2 === 0 ? '-10deg' : '10deg' })`,
+                opacity: 0
+            };
+            $this.css({
+                ...transformProperties,
+                transition: 'transform 1.5s ease-out, opacity 1.5s ease-out'
+            });
+        });
+    }
+
+    animateElements();
+    $(window).scroll(animateElements);
+
+    
